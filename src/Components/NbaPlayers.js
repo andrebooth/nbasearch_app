ae00a72404133bfe83683362bb08b7f8449f8a1d
Andre Booth <DreLeRoy@MacBook-Pro.home>
2022-05-17 13:31:10 -0700

    practicing fetching data with fetch

--- src/Components/NbaPlayers.js
+++ src/Components/NbaPlayers.js
@@ -2,27 +2,40 @@ import React from 'react'
 import Form from './Form';
 import {useState, useEffect } from 'react'
 
+
 function NbaPlayers () {
 
-  const [nbaList, setNbaList]= useState([])
-  const [searchbaPlayer, setSearchNbaPlayer] = useState('');
+  const [searchPlayer, setSearchPlayer] = useState("");
   
-    
+  const [playerList, setPlayerList]= useState(null)
 
-      useEffect(()=>{
-        fetch("https://www.balldontlie.io/api/v1/players?search=Jason")
+      useEffect(() => {
+        fetch("https://www.balldontlie.io/api/v1/players?search=lebron")
         .then(res => res.json())
         .then(data => console.log(data))
       }, [])
 
      
-    
+     
+    let playerDisplay = ""
+   
+   if(playerList !== null)
+    playerDisplay = (
+      <div>
+        <h2>Name { }</h2>
+      </div>
+    )
 
     return (
-      <>
+      
+      <div>
         <h2>NBA Players</h2>
-        <Form/>
-       </>
+        <Form />
+       
+
+        
+       </div>
+      
 
     )
   }
