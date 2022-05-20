8c72c94b69cbb6fecccfba6cccaa74ea39486af5
Andre Booth <DreLeRoy@MacBook-Pro.home>
2022-05-20 14:31:19 -0700

    final push

--- src/Components/Finals.js
+++ src/Components/Finals.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Finals() {
   return (
     <div>
-        <h1>Don't forget to watch the NBA FINALS on ABC June 2th</h1>
+        <h1>Don't forget to watch the NBA FINALS on ABC June 2nd</h1>
     </div>
   )
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,10 +11,10 @@ function Home() {
         <>
         
         <img className="Heat"src={Heat} alt = "Heat Logo" />
-              <img className="Celtics"src={Celtics} alt = "Celtics Logo" />
-              <img className="nba_logo"src={image} alt = "NBA Logo" />
-              <img className="Warriors"src={Warriors} alt = "Warriors Logo" />
-              <img className="Mavericks"src={Mavericks} alt = "Mavericks Logo" />
+        <img className="Celtics"src={Celtics} alt = "Celtics Logo" />
+        <img className="nba_logo"src={image} alt = "NBA Logo" />
+        <img className="Warriors"src={Warriors} alt = "Warriors Logo" />
+        <img className="Mavericks"src={Mavericks} alt = "Mavericks Logo" />
               <h2>Welcome, Click on the NBA Players Link in the header to get started.</h2>
         </>
     )
